98c7f54752d43640c551089d9b65fd9d6e8e58ce
vernala <ookkxw@163.com>
2019-11-07 10:02:02 +0800

    修复教员列表科目筛选

--- components/Student/SubjectFilterModal/SubjectFilterModal.js
+++ components/Student/SubjectFilterModal/SubjectFilterModal.js
@@ -12,15 +12,11 @@ Component({
         console.log(show, old)
         if (show && !old && !this.loaded) {
           this.loaded = true
-          http.postPromise('/teacher/listSubject', {}).then(data => {
-
+          http.postPromise('/teacher/findAllSubject', {}).then(data => {
+            this.setData({ list: [{ key: 0, value: '全部' }, ...data.data] })
           })
         }
       }
-    },
-    subjects: {
-      type: Array,
-      value: []
     },
   },
 
@@ -30,17 +26,43 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    list: [],
+    subjects: {
+      0: [
+        { key: 0, value: '全部' }
+      ]
+    },
+    subject: 0,
+    level: 0,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    fetchSubject(e) {
+      const id = Number(e.currentTarget.dataset.id)
+      const { subjects } = this.data
+      //console.log(subjects)
+      if (!subjects[id]) {
+        http.postPromise('/teacher/findAllSubject', { teachLevel: id }).then(data => {
+          subjects[id] = data.data
+          this.setData({ subjects, level: id, subject: 0 })
+        })
+      } else {
+        this.setData({ level: id, subject: 0 })
+      }
+    },
 
+
+    onChange(e) {
+      const id = Number(e.currentTarget.dataset.id)
+      this.setData({ subject: id })
+      this.triggerEvent('onChange', { subjectId: id || null })
+    },
   },
-
+  
   attached() {
-    
+
   }
 })
