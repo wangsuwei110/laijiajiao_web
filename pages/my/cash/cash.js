f1ad9e70d05c75d3e21307296bc66f0635cb7f10
vernala <ookkxw@163.com>
2019-11-21 15:49:16 +0800

    提交测试代码

--- pages/my/cash/cash.js
+++ pages/my/cash/cash.js
@@ -17,17 +17,47 @@ Page({
     //   })
     // })
   },
-  getMoney (e) {
+  getMoney(e) {
     this.setData({
       cashOut: e.detail.value
     })
   },
-  saveBtn () {
+  saveBtn() {
     var that = this
     wx.login({
       success(res1) {
         if (res1.code) {
-          http.post('/wxRedPack/sendRedPack', {teacherId: wx.getStorageSync('user_id'), code:  res1.code, cashOut: that.data.cashOut}, function (res) {
+
+          http.postPromise('/user/getOpenId', { code: res1.code }).then(data => {
+            //this.login(data.data.openid, gender)
+            
+            http.post('/wxRedPack/sendRedPack', { teacherId: wx.getStorageSync('user_id'), code: res1.code, cashOut: that.data.cashOut, openid: data.data.openid }, function (res) {
+              console.log(res.data)
+              var data = res.data
+              if (res.code === '200') {
+                console.log(data, '111111')
+              } else {
+                wx.showToast({
+                  title: res.msg,
+                  icon: 'none'
+                })
+              }
+            }, function (err) {
+              console.log(err)
+            }, function () {
+              wx.hideLoading()
+            })
+
+          }).catch(err => {
+            err && wx.showToast({
+              title: err.msg,
+              icon: 'none'
+            })
+          })
+          
+          return false
+
+          http.post('/wxRedPack/sendRedPack', { teacherId: wx.getStorageSync('user_id'), code: res1.code, cashOut: that.data.cashOut }, function (res) {
             console.log(res.data)
             var data = res.data
             if (res.code === '200') {
@@ -50,6 +80,6 @@ Page({
       }
     })
     // 确认提现
-   
+
   }
 })
